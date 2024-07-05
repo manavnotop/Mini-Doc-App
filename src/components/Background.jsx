8b54aa75449950efda64b8aadb40311e1e182c75
manavnotop <manuagarwal1443@gmail.com>
2024-07-06 01:07:19 +0530

    add background component

---
+++ src/components/Background.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Background() {
+  return (
+    <>
+    <div className='absolute top-[5%] w-full py-10 text-zinc-600 font-semibold text-xl flex justify-center'>
+        Documents.
+    </div>
+    <h1 className='absolute text-[13vw] -translate-x-[50%] -translate-y-[50%] top-1/2 left-1/2 leading-none tracking-tight font-semibold text-zinc-900'>
+        mnop.
+    </h1>
+    </>
+  )
+}
+
+export default Background
