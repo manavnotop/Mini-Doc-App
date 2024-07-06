f0548e21a688be639cf8164e77f8b70ce8bbe980
manavnotop <manuagarwal1443@gmail.com>
2024-07-06 14:45:01 +0530

    add card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='w-60 h-72 bg-zinc-900/90 rounded-[20px]'></div>
+  )
+}
+
+export default Card
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Card from './Card'
 
 function Foreground() {
   return (
-    <div className='h-full w-full top-0 left-0 z-[3] fixed bg-blue-600/20'>
-        
+    <div className='h-full w-full top-0 left-0 z-[3] fixed'>
+        <Card />
     </div>
   )
 }
