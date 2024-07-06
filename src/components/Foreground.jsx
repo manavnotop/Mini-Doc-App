60a45e79c71e9ea00f341c6cd15c8a1e5ac1b53e
manavnotop <manuagarwal1443@gmail.com>
2024-07-06 16:05:00 +0530

    add sample data for dynamic render

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import Card from './Card'
 
 function Foreground() {
+  const data = [{
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    fileSize: ".9mb",
+    close: true,
+    tag: {isOpen: true, tagTitle: "Download Now", tagColor: "green"}
+  }]
+  
   return (
     <div className='h-full w-full top-0 left-0 z-[3] fixed'>
         <Card />
