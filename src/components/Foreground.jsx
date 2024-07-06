eaaf43fb4b0262c25cf82a30104d3e01f36d88bc
manavnotop <manuagarwal1443@gmail.com>
2024-07-06 14:36:25 +0530

    add foreground component

---
+++ src/components/Foreground.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Foreground() {
+  return (
+    <div className='h-full w-full top-0 left-0 z-[3] fixed bg-blue-600/20'>
+        
+    </div>
+  )
+}
+
+export default Foreground
